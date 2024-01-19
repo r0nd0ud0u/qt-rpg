f501d094d4e99778d27b222d5101aaef19d07281
rondoudou <marcpon@yahoo.fr>
2024-01-19 16:04:19 +0100

    sonar comment

--- app/view/include/ApplicationView.h
+++ app/view/include/ApplicationView.h
@@ -5,6 +5,8 @@
 
 #include "mainwindow.h"
 
+#include <memory.h>
+
 class ApplicationView : public QObject
 {
     Q_OBJECT
@@ -12,14 +14,14 @@ public:
     static ApplicationView& GetInstance(); ///< Get singleton
     ~ApplicationView() override;
 
-    MainWindow* GetMainWindow() { return m_main_window; }
+    std::unique_ptr<MainWindow>& GetMainWindow() { return m_main_window; }
     void StartMainWindow();
 
 private:
     explicit ApplicationView(QObject* parent = nullptr);
-    void ShowWindow(QMainWindow* mw, bool show);
+    void ShowWindow(QMainWindow* mw, bool show) const;
 
-    MainWindow* m_main_window       = nullptr;
+    std::unique_ptr<MainWindow> m_main_window       = nullptr;
 };
 
 #endif // APPLICATIONVIEW_H
--- app/view/src/ApplicationView.cpp
+++ app/view/src/ApplicationView.cpp
@@ -1,42 +1,31 @@
 #include "ApplicationView.h"
 
-ApplicationView::ApplicationView(QObject* parent)
-    : QObject{parent}
-{
+ApplicationView::ApplicationView(QObject *parent) : QObject{parent} {}
+
+ApplicationView::~ApplicationView() {
+  if (m_main_window.get() != nullptr) {
+    m_main_window->hide();
+  }
 }
 
-ApplicationView::~ApplicationView()
-{
-    if (m_main_window != nullptr)
-    {
-        m_main_window->hide();
-    }
+ApplicationView &ApplicationView::GetInstance() {
+  static ApplicationView m_instance;
+  return m_instance;
 }
 
-ApplicationView& ApplicationView::GetInstance()
-{
-    static ApplicationView m_instance;
-    return m_instance;
+void ApplicationView::StartMainWindow() {
+  if (m_main_window == nullptr) {
+    m_main_window = std::make_unique<MainWindow>();
+    m_main_window->setWindowState(Qt::WindowMaximized);
+    m_main_window->show();
+  }
 }
 
-void ApplicationView::StartMainWindow()
-{
-    if (m_main_window == nullptr)
-    {
-        m_main_window = new MainWindow();
-        m_main_window->setWindowState(Qt::WindowMaximized );
-        m_main_window->show();
-    }
-
-}
-
-void ApplicationView::ShowWindow(QMainWindow* mw, bool show)
-{
-    if (mw != nullptr)
-    {
-        if (show)
-            mw->show();
-        else
-            mw->hide();
-    }
+void ApplicationView::ShowWindow(QMainWindow *mw, bool show) const {
+  if (mw != nullptr) {
+    if (show)
+      mw->show();
+    else
+      mw->hide();
+  }
 }
