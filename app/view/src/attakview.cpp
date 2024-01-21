606c4d726b84f15cfa7784419998f6b6fd3b6243
rondoudou <marcpon@yahoo.fr>
2024-01-21 13:38:16 +0100

    update attak view

--- app/view/src/attakview.cpp
+++ app/view/src/attakview.cpp
@@ -2,58 +2,43 @@
 #include "ui_attakview.h"
 
 #include "Application.h"
-#include "gamedisplay.h"
 
 #include <QPushButton>
-#include <QDebug>
 
-AttakView::AttakView(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::AttakView)
-{
-    ui->setupUi(this);
+AttakView::AttakView(QWidget *parent) : QWidget(parent), ui(new Ui::AttakView) {
+  ui->setupUi(this);
 }
 
-AttakView::~AttakView()
-{
-    delete ui;
+AttakView::~AttakView() { delete ui; }
+
+void AttakView::UpdateAttak(QString name) {
+  // remove current buttons
+  RemoveAttak();
+
+  const auto &app = Application::GetInstance();
+  if (app.m_GameManager == nullptr ||
+      app.m_GameManager->m_PlayersManager == nullptr ||
+      app.m_GameManager->m_PlayersManager->m_SelectedHero == nullptr) {
+    return;
+  }
+
+  for (const auto &atk :
+       app.m_GameManager->m_PlayersManager->m_SelectedHero->attakList) {
+    auto *button = new QPushButton();
+    button->setText(atk.name + " Mana " + QString::number(atk.manaCost) +
+                    " Effect: " + atk.effect);
+    ui->attak_lay->addWidget(button);
+  }
 }
 
-void AttakView::UpdateAttak(QString name){
-    RemoveAttak();
-
-    for (int i = 0; i < ui->attak_lay->count(); i++){
-        QWidget* widget = ui->attak_lay->itemAt(i)->widget();
-        if (widget != nullptr)
-        {
-            widget->setParent(nullptr);
-            ui->attak_lay->removeWidget(widget);
-            i--;
-        }
-
+void AttakView::RemoveAttak() {
+  for (int i = 0; i < ui->attak_lay->count(); i++) {
+    auto *widget = ui->attak_lay->itemAt(i)->widget();
+    if (widget == nullptr) {
+      continue;
     }
-    const auto &app = Application::GetInstance();
-    if (app.m_GameManager == nullptr ||
-        app.m_GameManager->m_PlayersManager == nullptr || app.m_GameManager->m_PlayersManager->m_SelectedHero == nullptr) {
-        return;
-    }
-
-    for (const auto& atk : app.m_GameManager->m_PlayersManager->m_SelectedHero->attakList ) {
-        auto* button = new QPushButton();
-        button->setText(atk.name + " Mana " + QString::number(atk.manaCost) + " Effect: " + atk.effect);
-        ui->attak_lay->addWidget(button);
-    }
+    widget->setParent(nullptr);
+    ui->attak_lay->removeWidget(widget);
+    i--;
+  }
 }
-
-void AttakView::RemoveAttak(){
-    auto itt = ui->attak_lay->count();
-    for (int i = 0; i < ui->attak_lay->count(); i++){
-        QWidget* widget = ui->attak_lay->itemAt(i)->widget();
-        if (widget != nullptr)
-        {
-            widget->setParent(nullptr);
-            ui->attak_lay->removeWidget(widget);
-            i--;
-        }
-
-    }}
