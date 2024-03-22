5fc174506e0ed9c96784b844dd8d230c7e6141f1
Marc Pontida <pontidmc@skysoft-atm.com>
2024-03-22 14:54:16 +0100

    first chain

--- app/core/src/main.cpp
+++ app/core/src/main.cpp
@@ -3,8 +3,22 @@
 
 #include "Stylizer.h"
 
+#include "rusty_bridge/lib.h"
+#include <iostream>
+#include <QtDebug>
+
+extern "C" {
+int32_t rusty_extern_c_integer();
+}
+
 int main(int argc, char *argv[])
 {
+    qDebug() << "A value given via generated cxxbridge "
+              << rusty_cxxbridge_integer() << "\n";
+    qDebug() << "A value given directly by extern c function "
+              << rusty_extern_c_integer() << "\n";
+
+    return 0;
     auto app = std::make_unique<Application>(argc, argv);
     app->Init();
 
