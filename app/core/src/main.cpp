572efe12bbfb404b43f0503f9a5c898ec3b21212
Marc Pontida <pontidmc@skysoft-atm.com>
2024-03-22 20:08:28 +0100

    update gha

--- app/core/src/main.cpp
+++ app/core/src/main.cpp
@@ -19,9 +19,9 @@ int main(int argc, char *argv[])
     qDebug() << "A value given directly by extern c function "
               << rusty_extern_c_integer() << "\n";
 
-rust::Vec<Shared> toto = rusty_cxxbridge_vector();
+    rust::Vec<Shared> toto = rusty_cxxbridge_vector();
    // rust::Vec<uint16_t> vec;
-//rust::Vec<Shared> toto = rusty_cxxbridge_vector();
+    //rust::Vec<Shared> toto = rusty_cxxbridge_vector();
 
     return 0;
     auto app = std::make_unique<Application>(argc, argv);
