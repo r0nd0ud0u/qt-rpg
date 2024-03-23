891673b609558074fd16b26e5fa795fd42804b12
marcopolux <pontidmc@skysoft-atm.com>
2024-03-23 11:31:04 +0100

    Add rust lib (#96)
    * remove unused flags
    
    * first chain
    
    * use mingw to use vec shared otherwise problem with static linking with msvc
    
    * update gha qt mingw 6.6.1

--- app/core/src/main.cpp
+++ app/core/src/main.cpp
@@ -1,10 +1,14 @@
 #include "Application.h"
 #include "ApplicationView.h"
-
 #include "Stylizer.h"
+
+#include "rusty_bridge/lib.h"
 
 int main(int argc, char *argv[])
 {
+    // test rust lib with vec
+    rust::Vec<Shared> toto = rusty_cxxbridge_vector();
+
     auto app = std::make_unique<Application>(argc, argv);
     app->Init();
 
