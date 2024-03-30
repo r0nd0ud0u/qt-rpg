407be4d25e761e79920c4fb31d838f63fe2900cd
r0nd0ud0u <57643172+r0nd0ud0u@users.noreply.github.com>
2024-03-30 14:41:22 +0100

    Use compiled lib (#97)
    * use submodules for rust-rpg
    
    * add utils from rust rpg

--- app/core/src/main.cpp
+++ app/core/src/main.cpp
@@ -2,12 +2,17 @@
 #include "ApplicationView.h"
 #include "Stylizer.h"
 
-#include "rusty_bridge/lib.h"
+#include "rust-rpg-bridge/lib.h"
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     // test rust lib with vec
+    Shared test;
+    test.v = 50;
     rust::Vec<Shared> toto = rusty_cxxbridge_vector();
+    test = toto.at(0);
+    qDebug() <<"" << test.v;
 
     auto app = std::make_unique<Application>(argc, argv);
     app->Init();
