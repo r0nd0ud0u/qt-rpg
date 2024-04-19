ae8b7ed4c75329d3b8e2e451b8c6fee42aa185e0
rondoudou <marcpon@yahoo.fr>
2024-04-19 11:09:30 +0200

    update win deploy script

--- scripts/deploy-release.sh
+++ scripts/deploy-release.sh
@@ -1,4 +1,4 @@
-WINDEPLOYQT="D:\\Qt\\5.15.2\\msvc2019_64\\bin\\windeployqt.exe"
-EXE_PATH="..\\build-qt_template-Desktop_Qt_5_15_2_MSVC2019_64bit-Release\\bin\\qt_template.exe"
+WINDEPLOYQT="D:\\Qt\\Qt6\\6.6.1\\msvc2019_64\\bin\\windeployqt.exe"
+EXE_PATH="..\\build-qt-rpg-Desktop_Qt_6_6_1_MSVC2019_64bit-Release\\bin\\qt-rpg.exe"
 
 "${WINDEPLOYQT}" "${EXE_PATH}"
