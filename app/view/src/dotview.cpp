57f3a49f97106021e31ccc2c69a9199b7f41231c
rondoudou <marcpon@yahoo.fr>
2024-05-03 22:56:19 +0200

    add greeen square for hot on hero panel

--- app/view/src/dotview.cpp
+++ app/view/src/dotview.cpp
@@ -6,6 +6,8 @@ DotView::DotView(QWidget *parent)
     , ui(new Ui::DotView)
 {
     ui->setupUi(this);
+    setStyleSheet(
+        "#hot_label{ background:     #00ff00;}");
 }
 
 DotView::~DotView()
