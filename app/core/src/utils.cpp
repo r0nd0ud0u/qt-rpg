7d1697fcd10afa964a366b3ca85a21c2ca262fd8
rondoudou <marcpon@yahoo.fr>
2024-05-19 16:40:14 +0200

    update random bug

--- app/core/src/utils.cpp
+++ app/core/src/utils.cpp
@@ -79,6 +79,8 @@ Utils::GetNbOfActiveEffects(const std::vector<GameAtkEffects> &table) {
 }
 
 int64_t Utils::GetRandomNb(const int64_t min, const int64_t max ){
-    srand((int)time(nullptr));
+    if(min == max){
+        return min;
+    }
     return (rand() % (max - min)) + 1;
 }
