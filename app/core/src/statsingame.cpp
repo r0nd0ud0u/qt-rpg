457370e8546b97a32d661651aa25e94add88a8b5
rondoudou <marcpon@yahoo.fr>
2024-08-20 21:21:28 +0200

    update review sonar

--- app/core/src/statsingame.cpp
+++ app/core/src/statsingame.cpp
@@ -6,8 +6,6 @@
 #include <QDir>
 #include <QFile>
 
-#include <numeric>
-
 void StatsInGame::GenerateStatsEndGame() {
   const auto *gm = Application::GetInstance().m_GameManager.get();
   if (gm == nullptr) {
@@ -31,10 +29,8 @@ void StatsInGame::GenerateStatsEndGame() {
   QTextStream out(&file);
   // title
   out << ENDGAME_TITLE_BAR.join(";\t") << "\n";
-  out << GetOutputEndGameByList(gm->m_PlayersManager->m_AllHeroesList).toUtf8()
-      << "\n";
-  out << GetOutputEndGameByList(gm->m_PlayersManager->m_AllBossesList).toUtf8()
-      << "\n";
+  out << GetOutputEndGameByList(pm->m_AllHeroesList).toUtf8() << "\n";
+  out << GetOutputEndGameByList(pm->m_AllBossesList).toUtf8() << "\n";
 }
 
 QString StatsInGame::GetOutputEndGameByList(
@@ -46,28 +42,31 @@ QString StatsInGame::GetOutputEndGameByList(
   QString output;
   const QString values = QStringList({"%1", "%2", "%3"}).join(";\t");
   for (const auto *h : playerList) {
-    if (h != nullptr && h->m_StatsInGame.m_IsPlaying) {
-      const auto type = (h->m_type == characType::Hero) ? "Hero" : "Boss";
-      const auto lifeStatus = h->m_StatsInGame.m_IsAlive ? "Alive" : "Dead";
-      output += values.arg(h->m_Name, type, lifeStatus) + "\n";
-      output += ";Attaks\n";
-      for (const auto &atk : h->m_AttakList) {
-        int nbOfUse = 0;
-        if (h->m_StatsInGame.m_AllAtksInfo.count(atk.first) > 0) {
-          nbOfUse = h->m_StatsInGame.m_AllAtksInfo.at(atk.first).nbOfUse;
-        }
-        output += QString(";;%1;%2").arg(atk.first).arg(nbOfUse) + "\n";
+    if (h == nullptr || h->m_StatsInGame.m_IsPlaying) {
+      continue;
+    }
+    const auto charaType = (h->m_type == characType::Hero) ? "Hero" : "Boss";
+    const auto lifeStatus = h->m_StatsInGame.m_IsAlive ? "Alive" : "Dead";
+    // update output with header
+    output += values.arg(h->m_Name, charaType, lifeStatus) + "\n";
+    // update output attacks
+    output += ";Attaks\n";
+    for (const auto &[atkName, atkType] : h->m_AttakList) {
+      int nbOfUse = 0;
+      if (h->m_StatsInGame.m_AllAtksInfo.count(atkName) > 0) {
+        nbOfUse = h->m_StatsInGame.m_AllAtksInfo.at(atkName).nbOfUse;
       }
-      // accumulate stats by turn RX TX
-      output += ";Stats RX TX\n";
-      for (int i = 0; i < h->m_LastTxRx.size(); i++) {
-        const auto &type = amountTypeStr[i];
-        uint64_t value = 0;
-        for (const auto &it : h->m_LastTxRx.at(i)) {
-          value += it.second;
-        }
-        output += QString(";;%1;%2").arg(type).arg(value) + "\n";
+      output += QString(";;%1;%2").arg(atkName).arg(nbOfUse) + "\n";
+    }
+    // update output with stats by turn RX TX
+    output += ";Stats RX TX\n";
+    for (int i = 0; i < h->m_LastTxRx.size(); i++) {
+      const auto &amountType = amountTypeStr[i];
+      uint64_t value = 0;
+      for (const auto &[turnNb, turnValue] : h->m_LastTxRx.at(i)) {
+        value += turnValue;
       }
+      output += QString(";;%1;%2").arg(amountType).arg(value) + "\n";
     }
   }
 
