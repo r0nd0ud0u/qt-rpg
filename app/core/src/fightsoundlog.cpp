aeeaffec5389c914dc20a1152b917e442a79b573
rondoudou <marcpon@yahoo.fr>
2024-11-24 23:23:30 +0100

    remove debug log

--- app/core/src/fightsoundlog.cpp
+++ app/core/src/fightsoundlog.cpp
@@ -26,21 +26,16 @@ void FightSoundLog::AddToQueue(const QString &soundName) {
   }
   QString soundPath = SOUNDS_DIR + soundName;
   m_Queue.enqueue(soundPath);
-  qDebug() << "add to queue-enqueue " << soundPath;
   if (!m_IsPlaying && !m_Queue.isEmpty()) {
-    qDebug() << "add to queue-play next " << soundPath;
     playNext();
   }
 }
 
 void FightSoundLog::onStateChanged(QMediaPlayer::PlaybackState state) {
-  qDebug() << "onStateChanged" << state;
   if (state == QMediaPlayer::StoppedState) {
-    qDebug() << "onStateChanged " << m_IsPlaying;
     m_IsPlaying = false;
   }
   if (state == QMediaPlayer::StoppedState && !m_Queue.isEmpty()) {
-    qDebug() << "onStateChanged-play next ";
     playNext();
   }
 }
@@ -48,16 +43,13 @@ void FightSoundLog::onStateChanged(QMediaPlayer::PlaybackState state) {
 void FightSoundLog::playNext() {
   if (!m_Queue.isEmpty()) {
     QString nextSound = m_Queue.dequeue();
-    qDebug() << "playNext " << nextSound;
     m_MediaPlayer->setSource(QUrl::fromLocalFile(nextSound));
     if (m_AudioOutput == nullptr || m_MediaPlayer->audioOutput() == nullptr) {
       m_AudioOutput = new QAudioOutput();
       m_MediaPlayer->setAudioOutput(m_AudioOutput);
       m_AudioOutput->setVolume(1.0f);
-      qDebug() << "set audiooutput ";
     }
     m_MediaPlayer->play();
     m_IsPlaying = true;
-    qDebug() << "playNext " << m_IsPlaying;
   }
 }
