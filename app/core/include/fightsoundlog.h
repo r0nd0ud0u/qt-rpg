3a6e52bc5f70d60001aca59091e07f9185efbaae
rondoudou <marcpon@yahoo.fr>
2024-11-24 16:18:34 +0100

    add queue to play sucessive sounds

--- app/core/include/fightsoundlog.h
+++ app/core/include/fightsoundlog.h
@@ -2,23 +2,40 @@
 #define FIGHTSOUNDLOG_H
 
 #include <QObject>
+#include <QQueue>
 
 #include <memory>
 
 #include <QAudioOutput>
 #include <QMediaPlayer>
 
-class FightSoundLog {
+class FightSoundLog : public QObject {
+  Q_OBJECT
 public:
-  FightSoundLog();
+  FightSoundLog(QObject *parent = nullptr)
+        : QObject(parent), m_MediaPlayer(new QMediaPlayer(this)), m_AudioOutput(new QAudioOutput(this)) {
+    // Connect to playbackStateChanged signal
+    connect(m_MediaPlayer, &QMediaPlayer::playbackStateChanged, this,
+            &FightSoundLog::onStateChanged);
+    m_MediaPlayer->setAudioOutput(m_AudioOutput);
+    m_AudioOutput->setVolume(1.);
+  }
   // logs
   static QString OutputDodge(const QString &name, const QString &randType);
   // sounds
   void PlayDodgingSound(const bool isBoss);
-  void LaunchSound(const QString &soundName);
+  void AddToQueue(const QString &soundName);
 
-  std::unique_ptr<QMediaPlayer> m_MediaPlayer = nullptr;
-  std::unique_ptr<QAudioOutput> m_AudioOutput = nullptr;
+private:
+  QMediaPlayer *m_MediaPlayer = nullptr;
+  QAudioOutput* m_AudioOutput = nullptr;
+  QQueue<QString> m_Queue;
+  bool m_IsPlaying = false;
+
+  void playNext();
+
+private slots:
+  void onStateChanged(QMediaPlayer::PlaybackState state);
 };
 
 #endif // FIGHTSOUNDLOG_H
--- app/core/src/fightsoundlog.cpp
+++ app/core/src/fightsoundlog.cpp
@@ -2,13 +2,6 @@
 
 #include "common.h"
 
-FightSoundLog::FightSoundLog() {
-  m_MediaPlayer = std::make_unique<QMediaPlayer>();
-  m_AudioOutput = std::make_unique<QAudioOutput>();
-  m_MediaPlayer->setAudioOutput(m_AudioOutput.get());
-  m_AudioOutput->setVolume(50.);
-}
-
 QString FightSoundLog::OutputDodge(const QString &name,
                                    const QString &randType) {
   return QString("%1 esquive.(%2)").arg(name, randType);
@@ -24,14 +17,47 @@ void FightSoundLog::PlayDodgingSound(const bool isBossDodging) {
   } else {
     soundName += "on-hero-dodging.ogg";
   }
-  LaunchSound(soundName);
+  AddToQueue(soundName);
 }
 
-void FightSoundLog::LaunchSound(const QString &soundName) {
+void FightSoundLog::AddToQueue(const QString &soundName) {
   if (soundName.isEmpty()) {
     return;
   }
   QString soundPath = SOUNDS_DIR + soundName;
-  m_MediaPlayer->setSource(soundPath);
-  m_MediaPlayer->play();
+  m_Queue.enqueue(soundPath);
+  qDebug() << "add to queue-enqueue " << soundPath;
+  if (!m_IsPlaying && !m_Queue.isEmpty()) {
+    qDebug() << "add to queue-play next " << soundPath;
+    playNext();
+  }
+}
+
+void FightSoundLog::onStateChanged(QMediaPlayer::PlaybackState state) {
+  qDebug() << "onStateChanged" << state;
+  if (state == QMediaPlayer::StoppedState) {
+    qDebug() << "onStateChanged " << m_IsPlaying;
+    m_IsPlaying = false;
+  }
+  if (state == QMediaPlayer::StoppedState && !m_Queue.isEmpty()) {
+    qDebug() << "onStateChanged-play next ";
+    playNext();
+  }
+}
+
+void FightSoundLog::playNext() {
+  if (!m_Queue.isEmpty()) {
+    QString nextSound = m_Queue.dequeue();
+    qDebug() << "playNext " << nextSound;
+    m_MediaPlayer->setSource(QUrl::fromLocalFile(nextSound));
+    if (m_AudioOutput == nullptr || m_MediaPlayer->audioOutput() == nullptr) {
+      m_AudioOutput = new QAudioOutput();
+      m_MediaPlayer->setAudioOutput(m_AudioOutput);
+      m_AudioOutput->setVolume(1.0f);
+      qDebug() << "set audiooutput ";
+    }
+    m_MediaPlayer->play();
+    m_IsPlaying = true;
+    qDebug() << "playNext " << m_IsPlaying;
+  }
 }
